07042cd79824f6f09ef6382bb9adf7ac816efebd
j-hamilton3 <jhamilton3@rrc.ca>
2024-02-27 14:57:17 -0600

    Added hierachical search for amiibo series.

--- app/controllers/amiibos_controller.rb
+++ app/controllers/amiibos_controller.rb
@@ -9,6 +9,16 @@ class AmiibosController < ApplicationController
 
   def search
     wildcard_search = "%#{params[:keywords]}%"
-    @amiibos = Amiibo.where("name LIKE ?", wildcard_search)
+    if params[:amiibo_series_id].present? && params[:amiibo_series_id] != ''
+      # Search within a specific Amiibo series
+      @amiibos = Amiibo.joins(:amiibo_series)
+                        .where("amiibo_series.id = ? AND amiibos.name LIKE ?",
+                                params[:amiibo_series_id], wildcard_search)
+      @category = AmiiboSeries.find(params[:amiibo_series_id]).name
+    else
+      # Search across all Amiibos
+      @amiibos = Amiibo.where("name LIKE ?", wildcard_search)
+      @category = "All Amiibos"
+    end
   end
 end
