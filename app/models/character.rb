2e46f665b257f78d6f1312586346efef29d4a5ad
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-11 17:01:28 -0500

    Added Characters & Game_Series CRUD.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,12 @@ class Character < ApplicationRecord
   has_many :amiibos
 
   validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_associations(auth_object = nil)
+    ["amiibos"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "name", "updated_at"]
+  end
 end
