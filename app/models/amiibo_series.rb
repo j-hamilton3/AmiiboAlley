d813fcbb199e3fc0e4f339af30fb6ae94619789e
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-11 16:53:13 -0500

    Added Amiibo_Series CRUD.

--- app/models/amiibo_series.rb
+++ app/models/amiibo_series.rb
@@ -3,4 +3,12 @@ class AmiiboSeries < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, length: { maximum: 300 }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["amiibos"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "name", "updated_at"]
+  end
 end
