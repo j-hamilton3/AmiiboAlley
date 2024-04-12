778637f460765957d0f16e0015fdb5886b1b06b8
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-11 21:14:41 -0500

    Added cart controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+  def initialize_session
+    session[:shopping_cart] ||= [] #Contains Amiibo Ids
+  end
+
+  def cart
+    Amiibo.find(session[:shopping_cart])
+  end
+
 end
