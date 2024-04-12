89316638038210d4cfae98d15721d0a5bb0631b7
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-11 21:43:51 -0500

    Cart functionality working.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,9 @@
 class CartController < ApplicationController
   def create
-    logger.debug("adding: #{params[:shopping_cart]}")
+    logger.debug("adding: #{params[:id]}")
+    id = params[:id].to_i
+    session[:shopping_cart] << id
+    redirect_to root_path
   end
 
   def destroy
