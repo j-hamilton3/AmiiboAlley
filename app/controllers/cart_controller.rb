f6425d20e9547c152f3d4cf5223cb9abb1b512e8
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-11 22:01:04 -0500

    Added flash message.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,9 @@ class CartController < ApplicationController
   def create
     logger.debug("adding: #{params[:id]}")
     id = params[:id].to_i
-    session[:shopping_cart] << id
+    session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
+    amiibo = Amiibo.find(id)
+    flash[:notice] = "#{amiibo.name} was added to the cart. 🛒"
     redirect_to root_path
   end
 
