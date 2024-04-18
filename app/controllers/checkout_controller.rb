141a8f6437e5d89c613f07360a500a640baa0fdd
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-18 14:29:10 -0500

    Quantity editing working.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,27 +1,20 @@
 class CheckoutController < ApplicationController
   def create
-    # Establish a connection with Stripe and redirect the user to the payment screen
-
-    # Return if shopping cart is empty.
-    if params[:shopping_cart].nil?
+    if params[:quantities].blank?
       redirect_to root_path
       return
     end
 
-    shopping_cart = params[:shopping_cart].split
-    line_items = [] # Initialize an empty array for line items
+    # Convert the amiibo and quantities to a hash.
+    quantities = params[:quantities].permit!.to_h
 
-    # Iterate through each amiibo in the shopping cart
-    shopping_cart.each do |amiibo_id|
-      # Find the amiibo
+    line_items = quantities.map do |amiibo_id, quantity|
       amiibo = Amiibo.find(amiibo_id)
-
-      # Add the amiibo to the line items array
-      line_items << {
-        quantity: 1, # We will hardcode one for this demo.
+      {
+        quantity: quantity.to_i,
         price_data: {
           unit_amount: (amiibo.price * 100).to_i,
-          currency: "cad",
+          currency: 'cad',
           product_data: {
             name: amiibo.name,
             description: amiibo.amiibo_series.name
@@ -30,16 +23,14 @@ class CheckoutController < ApplicationController
       }
     end
 
-    # Create a Stripe Checkout Session with the line items
     @session = Stripe::Checkout::Session.create(
-      payment_method_types: ["card"],
+      payment_method_types: ['card'],
       success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: checkout_cancel_url,
-      mode: "payment",
-      line_items: line_items # Set the line items for the session
+      mode: 'payment',
+      line_items: line_items
     )
 
-    # Redirect to the session URL
     redirect_to @session.url, allow_other_host: true
   end
 
