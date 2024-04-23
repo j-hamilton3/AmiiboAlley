6d4a0bdc45cbe6f7d66b3e2b81fc04004b05d778
j-hamilton3 <jhamilton3@rrc.ca>
2024-04-23 12:50:52 -0500

    Added status to Orders.

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register Order do
+  permit_params :orderDate, :user_id, :status
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-   permit_params :orderDate, :user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:orderDate, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
+  form do |f|
+    f.semantic_errors(*(f.object.errors.attribute_names))
+    f.inputs 'Order Details' do
+      f.input :orderDate, as: :datepicker
+      f.input :user, as: :select, collection: User.all.map { |u| [u.email, u.id] }
+      f.input :status, as: :select, collection: Order.statuses.keys.map { |status| [status.titleize, status] }
+    end
+    f.actions
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,20 @@ class Order < ApplicationRecord
 
   validates :orderDate, presence: true
   validates :user_id, presence: true, numericality: true
+  validates :status, presence: true
+
+  enum status: {
+    new_status: 'new',
+    paid: 'paid',
+    shipped: 'shipped',
+    cancelled: 'cancelled'
+  }
 
   def self.ransackable_associations(auth_object = nil)
     ["order_amiibos", "user"]
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "id_value", "orderDate", "updated_at", "user_id"]
+    ["created_at", "id", "id_value", "orderDate", "status", "updated_at", "user_id"]
   end
 end
